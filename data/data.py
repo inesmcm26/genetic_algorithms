04810434a62b125436554876eff10b436e462b63
inesmcm <ines.magessi@tecnico.ulisboa.pt>
2023-05-01 13:13:58 +0100

    data for each optimization problem

---
+++ data/data.py
@@ -0,0 +1,9 @@
+# one dim. list to keep the city names
+cities = ["New York", "Los Angeles", "Chicago", "Minneapolis"]
+# two dim. list to keep the distance matrix
+distance_matrix = [
+
+    [0, 2451, 713, 1018],
+    [2451, 0, 1745, 1524],
+    [713, 1745, 0, 355],
+    [1018, 1524, 355, 0]]
